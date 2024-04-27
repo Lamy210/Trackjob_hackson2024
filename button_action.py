26c54218df0f30f1a6bbca01e24004a6b189d16f
Lamy210 <syoytaku@gmail.com>
2024-04-28 03:36:20 +0900

    コメント追加　マイク処理コメントアウト　gitinore追加

--- button_action.py
+++ button_action.py
@@ -4,12 +4,46 @@ import sounddevice as sd
 import time
 
 #音声処理
+#delay_entry, pitch_var, own_key_var, stream_key_var, own_interface_var, stream_interface_var
+#audio_path 音楽のファイルパス
+
+#音声キー処理
+def play_voice_processing():
+
+    print("play_voice_processing:start")
+
+
+
+
+
+
+
+    print("play_voice_processing:end")
+
 def play_action(delay_entry, pitch_var, own_key_var, stream_key_var, own_interface_var, stream_interface_var):
     print(f"Delay set to: {delay_entry.get()}")
     print(f"Pitch set to: {pitch_var.get()}")
     print(f"Own Key set to: {own_key_var.get()}")
     print(f"Stream Key set to: {stream_key_var.get()}")
     print(f"Stream Output Interface: {stream_interface_var.get()}")
+
+
+
+    #配信者側出力
+
+
+
+
+
+    #配信先出力
+
+
+
+
+
+
+
+
 
     '''
     #入出力インターフェース設定未
